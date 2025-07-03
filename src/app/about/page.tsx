4e5bc8e3ada59423585ffb384994211d4044cfa3
吉村直人 <ahowateto123@gmail.com>
2025-07-03 16:33:46 +0900

    client component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,16 @@
+'use client'
+
+import { useState } from "react";
+
 export default function Page() {
+
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-      Aboutページ
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
     </div>
   )
 }
